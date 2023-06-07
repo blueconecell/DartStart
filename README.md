2f75f962ef59102e4076f9eee7edb04340f08cec
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-06-07 23:46:28 +0900

    ~ #1.2 dynamic type

---
+++ README.md
@@ -0,0 +1,3 @@
+# DART 시작하기
+
+[노마드 코더 강의](https://nomadcoders.co/dart-for-beginners)
---
+++ main.dart
@@ -0,0 +1,7 @@
+void main() {
+  print('hello world');
+  dynamic name;
+  if(name is String){
+    name.
+  } 
+}
