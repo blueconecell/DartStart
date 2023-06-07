19dc40a097b74adf1dbcd92d6035417c1bae3a04
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-06-08 00:13:01 +0900

    #1.7 Recap

--- main.dart
+++ main.dart
@@ -1,9 +1,20 @@
 void main() {
   print('now Running Dart!!!');
 
-  const API = '1212222';
+  var a;
+  a = '123';
+  a = 123;
+  a = true;
+  print(a);
 
-  const Max_allowed_price = 120;
-  //앱스토어에 올리기 전에 알고있는 값 const
-  // 올리기전에 모르는 것(api로 불러온 값) final, var
+  dynamic b;
+  b = '1234';
+  b = 1234;
+  b = true;
+  print(b);
+
+  String? c;
+  c = '123';
+  c?.isEmpty;
+  print(c);
 }
