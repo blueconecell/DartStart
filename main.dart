410e60a18afc46d7f48af1b1b1b2d1e15f7aa922
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-06-08 00:06:31 +0900

    #1.6 Constant Variables

--- main.dart
+++ main.dart
@@ -1,19 +1,9 @@
 void main() {
   print('now Running Dart!!!');
 
-  late final String name;
-  // print(name);
+  const API = '1212222';
 
-  // do someThing, go to api
-
-  name = 'jeongyeon';
-
-  late final String otherName = 'perfect';
-  // otherName = 'not';
-
-  final String cant;
-  // print(cant);
-  cant = 'cant';
-
-  print(name + ' ' + otherName + ' ' + cant);
+  const Max_allowed_price = 120;
+  //앱스토어에 올리기 전에 알고있는 값 const
+  // 올리기전에 모르는 것(api로 불러온 값) final, var
 }
