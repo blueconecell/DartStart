a5b557d43265527f2ef87233d00fde0d2e61a70f
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-06-08 22:49:58 +0900

    #2.4 Maps

--- main.dart
+++ main.dart
@@ -1,10 +1,29 @@
 void main() {
-  var oldFriends = ['nice', 'lynn'];
-  var newFriends = [
-    'lewis',
-    'ralph',
-    'darren',
-    for (var friend in oldFriends) "💕 $friend",
+  var player = {
+    'name': 'nice',
+    'xp': 19.99,
+    'superpower': false,
+  };
+  Map<int, bool> roll = {
+    1: true,
+    2: false,
+    3: true,
+  };
+  Map<List<int>, bool> roll2 = {
+    [1]: true,
+    [2]: false,
+    [3, 23]: true,
+  };
+  List<Map<String, Object>> playerList = [
+    {
+      'name': 'nice',
+      'xp': 19.99,
+      'superpower': false,
+    },
+    {
+      'name': 'nice',
+      'xp': 19.99,
+      'superpower': false,
+    },
   ];
-  print(newFriends);
 }
