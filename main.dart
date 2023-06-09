5e0a977bda57768a2fb2b39eb842a1a67e31c32e
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-06-09 13:57:23 +0900

    #3.5 Typedef

--- main.dart
+++ main.dart
@@ -1,24 +1,17 @@
-String capitalizeName(String? name) {
-  if (name != null) {
-    return name.toUpperCase();
-  }
-  return "none...";
+typedef ListOfInts = List<int>;
+
+List<int> reverseListOfNumbers(List<int> list) {
+  var reversed = list.reversed;
+  return reversed.toList();
 }
 
-String capitalizeName2(String? name) => name?.toUpperCase() ?? "none.....";
-// name != null ? name.toUpperCase() : "none.....";
+typedef UserInfo = Map<String, String>;
+
+String sayHi(Map<String, String> userInfo) {
+  return "Hi ${userInfo['name']}";
+}
 
 void main() {
-  print(capitalizeName('kim'));
-  print(capitalizeName(null));
-  print(capitalizeName2('kim'));
-  print(capitalizeName2(null));
-
-  String? name;
-
-  // 만약 name 이 null 이라면 nico라는 값을 넣어라
-  name ??= 'nico';
-  name = null;
-  name ??= 'other';
-  print(name);
+  print(reverseListOfNumbers([1, 2, 3]));
+  print(sayHi({"name": "kim"}));
 }
