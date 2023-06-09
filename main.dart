da0163d0d9a5297162b8102066ff23393443f274
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-06-09 13:33:52 +0900

    #3.1 Named Parameters

--- main.dart
+++ main.dart
@@ -1,5 +1,18 @@
 void main() {
-  print(sayHello('kim'));
+  print(sayHello1(age: 22, name: 'kim', country: 'cuba'));
+  print(sayHello1());
+  print(sayHello2(age: 22, name: 'kim', country: 'cuba'));
+  //print(sayHello2());
 }
 
-String sayHello(String name) => "Hello $name nice to meet you!";
+//default values
+String sayHello1(
+    {String name = 'anon', int age = 0, String country = 'wakanda'}) {
+  return "Hello $name, you are $age you, and you come from $country";
+}
+
+//require modifier
+String sayHello2(
+    {required String name, required int age, required String country}) {
+  return "Hello $name, you are $age you, and you come from $country";
+}
