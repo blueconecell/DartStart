cea0e5848ad0b71761bd8ed6dfd39e2ea6273f9c
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-06-09 13:22:53 +0900

    #3.0 Defining a Function

--- main.dart
+++ main.dart
@@ -1,11 +1,5 @@
 void main() {
-  var numbers = {1, 2, 3, 4};
-  Set<int> numbersSet = {1, 2, 3, 4}; //내부 숫자들은 유니트해진다.
-  numbersSet.add(1);
-  numbersSet.add(1);
-  numbersSet.add(1);
-  numbersSet.add(11);
-  numbersSet.add(11);
-  numbersSet.add(11);
-  print(numbersSet);
+  print(sayHello('kim'));
 }
+
+String sayHello(String name) => "Hello $name nice to meet you!";
