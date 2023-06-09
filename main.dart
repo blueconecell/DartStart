e8c7973113fb58b6929c72f6cb00dcb81b0e79a8
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-06-09 13:40:36 +0900

    #3.2 Recap, #3.3 Optional Positional Parameters

--- main.dart
+++ main.dart
@@ -1,18 +1,7 @@
+String sayHello(String name, int age, [String? country = 'cuba']) {
+  return "Hello $name, you are $age, you come from $country";
+}
+
 void main() {
-  print(sayHello1(age: 22, name: 'kim', country: 'cuba'));
-  print(sayHello1());
-  print(sayHello2(age: 22, name: 'kim', country: 'cuba'));
-  //print(sayHello2());
-}
-
-//default values
-String sayHello1(
-    {String name = 'anon', int age = 0, String country = 'wakanda'}) {
-  return "Hello $name, you are $age you, and you come from $country";
-}
-
-//require modifier
-String sayHello2(
-    {required String name, required int age, required String country}) {
-  return "Hello $name, you are $age you, and you come from $country";
+  print(sayHello('kim', 12));
 }
