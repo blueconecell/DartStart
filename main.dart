2b0b9a6a3f1f426084c2dbec561bb76e36501a15
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-06-09 13:49:16 +0900

    #3.4 QQ Operator

--- main.dart
+++ main.dart
@@ -1,7 +1,24 @@
-String sayHello(String name, int age, [String? country = 'cuba']) {
-  return "Hello $name, you are $age, you come from $country";
+String capitalizeName(String? name) {
+  if (name != null) {
+    return name.toUpperCase();
+  }
+  return "none...";
 }
 
+String capitalizeName2(String? name) => name?.toUpperCase() ?? "none.....";
+// name != null ? name.toUpperCase() : "none.....";
+
 void main() {
-  print(sayHello('kim', 12));
+  print(capitalizeName('kim'));
+  print(capitalizeName(null));
+  print(capitalizeName2('kim'));
+  print(capitalizeName2(null));
+
+  String? name;
+
+  // 만약 name 이 null 이라면 nico라는 값을 넣어라
+  name ??= 'nico';
+  name = null;
+  name ??= 'other';
+  print(name);
 }
