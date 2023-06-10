d33af819a78d83cd2dbbeb7f254d179480b222bd
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-06-10 15:49:26 +0900

    #4.4 Recap

--- main.dart
+++ main.dart
@@ -1,26 +1,12 @@
 class Player {
   final String name; //cant change
-  int xp, age;
+  int xp;
   String team;
 
-  Player({
-    required this.name,
-    required this.xp,
-    required this.team,
-    required this.age,
-  });
-  Player.createBluePlayer({required String name, required int age})
-      : this.age = age,
-        this.name = name,
-        this.team = 'blue',
-        this.xp = 0;
-  // 초기화시키기
-  Player.createRedPlayer({required String name, required int age})
-      : this.age = age,
-        this.name = name,
-        this.team = 'red',
-        this.xp = 0;
-  // 초기화시키기
+  Player.fromJSON(Map<String, dynamic> playerJOSN)
+      : name = playerJOSN['name'],
+        xp = playerJOSN['xp'],
+        team = playerJOSN['team'];
 
   void sayHello() {
     print("hi my name is $name");
@@ -28,15 +14,26 @@ class Player {
 }
 
 void main() {
-  print('123');
+  var apiData = [
+    {
+      "name": "nico",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "kim",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "lee",
+      "team": "blue",
+      "xp": 0,
+    },
+  ];
 
-  var bluePlayer1 = Player.createBluePlayer(
-    name: 'kim',
-    age: 12,
-  );
-
-  var redPlayer1 = Player.createRedPlayer(
-    name: 'park',
-    age: 12,
-  );
+  apiData.forEach((playerJSON) {
+    var player = Player.fromJSON(playerJSON);
+    player.sayHello();
+  });
 }
