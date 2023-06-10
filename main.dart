bd25370fda1f3d3420e3461d4e2d10c08baf8942
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-06-10 15:57:38 +0900

    #4.5 Cascade Notation

--- main.dart
+++ main.dart
@@ -1,39 +1,22 @@
 class Player {
-  final String name; //cant change
+  String name; //cant change
   int xp;
   String team;
 
-  Player.fromJSON(Map<String, dynamic> playerJOSN)
-      : name = playerJOSN['name'],
-        xp = playerJOSN['xp'],
-        team = playerJOSN['team'];
-
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
   void sayHello() {
     print("hi my name is $name");
   }
 }
 
 void main() {
-  var apiData = [
-    {
-      "name": "nico",
-      "team": "red",
-      "xp": 0,
-    },
-    {
-      "name": "kim",
-      "team": "red",
-      "xp": 0,
-    },
-    {
-      "name": "lee",
-      "team": "blue",
-      "xp": 0,
-    },
-  ];
-
-  apiData.forEach((playerJSON) {
-    var player = Player.fromJSON(playerJSON);
-    player.sayHello();
-  });
+  var kim = Player(name: 'kim', xp: 1200, team: 'red')
+    ..name = 'las'
+    ..xp = 90
+    ..team = 'blue'
+    ..sayHello();
 }
